102d3161775b342656ca828a870f89447ffc038d
ax8888 <adriana0villarroel@gmail.com>
2023-06-07 10:25:23 -0400

    COM008: fixed formatting for testing syntax analysis

--- test_syntax_analysis.py
+++ test_syntax_analysis.py
@@ -1,6 +1,7 @@
 from Gramática.GLC import GLC
 from Análisis_Sintáctico import syntax_chart
 
+#Grammar to test in simplified form
 glc = GLC('A')
 glc.add_production('A', "B C'")
 glc.add_production("C'", "+ B C'")
@@ -10,30 +11,16 @@ glc.add_production('G', "* F G")
 glc.add_production('G', "λ")
 glc.add_production('F', "a")
 glc.add_production('F', "( A )")
+######
 
-#glc.get_first()
-#glc.get_following()
-
-glc.firstS = {
-    'A': ['a', '('], 
-    "C'": ['+', 'λ'], 
-    'B': ['a', '('],
-    'G': ['*', 'λ'],
-    'F': ['a', '('] 
-    }
-
-glc.followingS = {
-    'A': ['$', ')'], 
-    "C'": ['$', ')'], 
-    'B': ['+', '$', ')'],
-    'G': ['+', '$', ')'],
-    'F': ['*', '+', '$', ')'] 
-    }
-
-
+glc.get_first()
+print("Firsts:", glc.firstS)
+glc.get_following()
+print("Followings:", glc.followingS)
+print("Firsts:", glc.firstS)
 
 chart = syntax_chart.createChart(glc)
 syntax_chart.printChart(glc.terminals, glc.nonTerminals, chart)
-#syntaxtchart.parse("(a+a)*a", chart, glc)
+syntax_chart.parse("( a + a ) * a", chart, glc)
 syntax_chart.parse("( a ) * ( a + a )", chart, glc)
-#syntaxtchart.parse("a", chart, glc)
+syntax_chart.parse("a", chart, glc)
